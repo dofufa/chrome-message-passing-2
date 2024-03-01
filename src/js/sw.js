3e114024911a374a6dbf81f8a7c63eb1d1b1210e
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2024-03-01 11:28:05 -0800

    Update sw.js

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,18 +1,17 @@
 // sw.js
-// job	: Receives message from popup context
+// job	: Receives message from content script context
 // lic 	: https://opensource.org/license/mit
-// repo : https://github.com/dofufa/chrome-message-passing-1
+// repo : https://github.com/dofufa/chrome-message-passing-2
 console.log('ext-service-worker');
 
-chrome.runtime.onMessage.addListener(
-	function (request, sender, sendResponse) {
-		const reply = {
-			name: 'from-service-worker',
-			sriracha: true
-		};
-	  console.log('message received by service worker, from popup')
-		console.log('sender: %o', sender);
-		console.log('request: %o', request);
-    console.log();
-		sendResponse(reply);
+chrome.action.onClicked.addListener((tab) => {
+	const message = { name: 'from-service-worker', greeting:'howdy!' };
+	console.log(tab);
+	console.log(tab.id);
+	console.log(message);
+	chrome.tabs.sendMessage(tab.id, {"message": message}, (response) => {
+  	console.log('response received by service-worker, from content-script')
+  	console.log('response: %o', response);
+		console.log();
 	});
+});
